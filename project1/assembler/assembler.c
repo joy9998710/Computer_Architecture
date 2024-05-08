6c781066952e0abcbf58497a358176058a1b381f
2022006135 <joy999871@hanyang.ac.kr>
2024-05-08 14:01:57 +0900

    Assembler and simulator

--- project1/assembler/assembler.c
+++ project1/assembler/assembler.c
@@ -5,20 +5,17 @@
 #include <string.h>
 
 #define MAXLINELENGTH 1000
-/* Define max label number as 1000 As this is a small program*/
+/* Define max label number as 1000 */
 #define MAXLABELNUM 1000
 
-/* label container */
 struct Label{
 	char *name;
 	int address;
+	int value; /* Value that label contains */
 	char *label;
-	int value;
 };
 
 struct Label* Labels[MAXLABELNUM];
-
-/* label num */
 int labelnum;
 
 int readAndParse(FILE *, char *, char *, char *, char *, char *);
@@ -28,12 +25,10 @@ int RTypeFormat(char *, char *, char *, char *);
 int ITypeFormat(char *, char *, char *, char *, int);
 int JTypeFormat(char *, char *, char *);
 int OTypeFormat(char *);
+void initLabel(FILE *, char *, char *, char *, char *, char *);
 int isValidLabel(char *);
-int isLetter(char *);
-void initlabel(FILE *, char *, char *, char *, char *, char *);
-int isValidNumber(long long);
-int getLabelValue(char *);
 int getLabelAddress(char *);
+int getLabelValue(char *);
 
 int main(int argc, char *argv[]) 
 {
@@ -61,53 +56,52 @@ int main(int argc, char *argv[])
 		printf("error in opening %s\n", outFileString);
 		exit(1);
 	}
-
-	/* TODO: Phase-1 label calculation */
-	/* Initialize Labels */
-	/* Program counter */
 	int pc = 0;
-	initlabel(inFilePtr, label, opcode, arg0, arg1, arg2);
+	initLabel(inFilePtr, label, opcode, arg0, arg1, arg2);
 
-	/* TODO: Phase-2 generate machine codes to outfile */
 	while(readAndParse(inFilePtr, label, opcode, arg0, arg1, arg2)){
-		int machine_code;
-		/* if opcode is R-Type */
-		if(!strcmp(opcode, "add") || ! strcmp(opcode, "nor")){
+		int machine_code = 0;
+		/* R-Type */
+		if(!strcmp(opcode, "add")){
 			machine_code = RTypeFormat(opcode, arg0, arg1, arg2);
 		}
-		/* if opcode is I-Type */
+		/* I-Type */
 		else if(!strcmp(opcode, "lw") || !strcmp(opcode, "sw") || !strcmp(opcode, "beq")){
 			machine_code = ITypeFormat(opcode, arg0, arg1, arg2, pc);
 		}
-		/* if opcode is J-Type */
+		/* J-Type */
 		else if(!strcmp(opcode, "jalr")){
 			machine_code = JTypeFormat(opcode, arg0, arg1);
 		}
-		/* if opcode is O-Type */
+		/* O-Type */
 		else if(!strcmp(opcode, "halt") || !strcmp(opcode, "noop")){
 			machine_code = OTypeFormat(opcode);
 		}
+		/* */
 		else if(!strcmp(opcode, ".fill")){
 			if(isNumber(arg0)){
-				machine_code = atoi(arg0);
+				machine_code = getLabelValue(label);
 			}
 			else{
-				machine_code = getLabelValue(label);
+				machine_code = getLabelAddress(arg0);
 			}
 		}
 		else{
-			printf("Unrecognized opcode");
+			printf("Undefined opcode\n");
 			exit(1);
 		}
-		fprintf(outFilePtr, "(address %d): %d (hex 0x%x)\n", pc, machine_code, machine_code);
+		fprintf(outFilePtr, "%d\n", machine_code);
+		printf("(address %d): %d (hex 0x%x)\n", pc, machine_code, machine_code);
 		pc++;
 	}
 
 	for(int i = 0; i < MAXLABELNUM; i++){
-		free(Labels[i]->name);
-		free(Labels[i]->label);
+		if(Labels[i]->name != NULL){
+			free(Labels[i]->name);
+		}
 		free(Labels[i]);
 	}
+
 
 	if (inFilePtr) {
 		fclose(inFilePtr);
@@ -175,7 +169,6 @@ int isNumber(char *string)
 }
 
 int isValidReg(char *reg){
-	/* return 1 if register is valid*/
 	if(!isNumber(reg)){
 		return 0;
 	}
@@ -186,18 +179,18 @@ int isValidReg(char *reg){
 	return 1;
 }
 
-/* RType format instruction to machine code */
-/* and, nor */
-int RTypeFormat(char *opcode, char *reg0, char *reg1, char *destReg){
-	/* As the machine code is 4 byte declare it as an int value*/
+/* R type format instruction to machine code */
+/* and | nor */
+int RTypeFormat(char *opcode, char *reg1, char *reg2, char *destReg){
 	int machine_code = 0;
-	/* Check if registers are valid*/
-	if(!(isValidReg(reg0)) || !(isValidReg(reg1)) || !(isValidReg(destReg))){
+	if(!(isValidReg(reg1) || !(isValidReg(reg2)))){
 		printf("Register is not valid");
 		exit(1);
 	}
+
 	/* Opcode: 24-22 bits */
 	/* add 000 */
+
 	if(!strcmp(opcode, "add")){
 		machine_code = (0 << 22);
 	}
@@ -206,25 +199,23 @@ int RTypeFormat(char *opcode, char *reg0, char *reg1, char *destReg){
 		machine_code = (1 << 22);
 	}
 
-	/* reg 0 21-19 bits */
-	int temp = atoi(reg0);
+	/* reg1 21-19 bits */
+	int temp = atoi(reg1);
 	machine_code |= (temp << 19);
-	/* reg 1 18-16 bits */
-	temp = atoi(reg1);
+	/* reg2 18-16 bits */
+	temp = atoi(reg2);
 	machine_code |= (temp << 16);
-	/* destReg 2-0 bits*/
+	/* destReg 2-0 bits */
 	temp = atoi(destReg);
-	machine_code |= (temp);
+	machine_code |= temp;
 
 	return machine_code;
 }
 
-/* I type format instruction to machine code */
-/* lw, sw, beq */
-int ITypeFormat(char *opcode, char *reg0, char *reg1, char *offsetField, int pc){
+int ITypeFormat(char *opcode, char *reg1, char *reg2, char *offsetField, int pc){
 	int machine_code = 0;
-	if(!(isValidReg(reg0)) || !(isValidReg(reg1))){
-		printf("Register is not valid");
+	if(!(isValidReg(reg1)) || !(isValidReg(reg2))){
+		printf("Register is not valid\n");
 		exit(1);
 	}
 
@@ -242,71 +233,74 @@ int ITypeFormat(char *opcode, char *reg0, char *reg1, char *offsetField, int pc)
 		machine_code = (4 << 22);
 	}
 
-	/* reg0 : 21- 19 bits */
-	int temp = atoi(reg0);
+	/* reg1 : 21-19 bits */
+	int temp = atoi(reg1);
 	machine_code |= (temp << 19);
-	/* reg1 : 18-16 bits */
-	temp = atoi(reg1);
+	/* reg2 : 18-16 bits */
+	temp = atoi(reg2);
 	machine_code |= (temp << 16);
+
+	/* offset */
 
 	temp = 0;
 	if(isNumber(offsetField)){
 		temp = atoi(offsetField);
 
+		/* Check valid offset */
 		if(temp > 32767 || temp < -32768){
-			printf("offset out of range");
+			printf("Offset out of range\n");
 			exit(1);
 		}
 	}
 	else{
-		temp = getLabelAddress(offsetField);
-	}
-
-
-	if(!strcmp(opcode, "beq") && !isNumber(offsetField)){
-		//using offset 
-		temp -= pc - 1;
-		if(temp > 32787 || temp < -73788){
-			printf("offset out of range");
+		if(!strcmp(opcode, "lw")){
+			temp = getLabelAddress(offsetField);
+		}
+		else if(!strcmp(opcode, "sw")){
+			temp = getLabelAddress(offsetField);
+		}
+		else if(!strcmp(opcode, "beq")){
+			temp = getLabelAddress(offsetField);
+			temp -= pc + 1;
+		}
+		/* check valid offset */
+		if(temp > 32767 || temp < -3268){
+			printf("Offset out of range\n");
 			exit(1);
 		}
 	}
-	/* Address is 16 bits */
 	temp &= 0xFFFF;
 	machine_code |= temp;
-
 	return machine_code;
 }
 
 /* J type format instruction to machine code */
 /* jalr */
-int JTypeFormat(char *opcode, char *reg0, char *reg1){
+int JTypeFormat(char *opcode, char *reg1, char *reg2){
 	int machine_code = 0;
-	if(!(isValidReg(reg0)) || !(isValidReg(reg1))){
-		printf("Register is not valid");
+	if(!(isValidReg(reg1)) || !(isValidReg(reg2))){
+		printf("Register is not valid\n");
 		exit(1);
 	}
 	/* Opcode: 24-22 bits */
-	/* jalr : 101 */
+	/* jalr: 101 */
 	if(!strcmp(opcode, "jalr")){
 		machine_code |= (5 << 22);
 	}
 
-	/* reg0: 21-19 bits */
-	int temp = atoi(reg0);
+	/* reg1 : 21019 bits */
+	int temp = atoi(reg1);
 	machine_code |= (temp << 19);
-	/* reg1: 18-16 */
-	temp = atoi(reg1);
+	/* reg2: 18-16 bits */
+	temp = atoi(reg2);
 	machine_code |= (temp << 16);
 
 	return machine_code;
 }
 
-/* O type format to machine code */
-/* halt, noop */
 int OTypeFormat(char *opcode){
 	int machine_code = 0;
-
+	
 	/* Opcode: 24-22 bits */
 	/* halt: 110 */
 	if(!strcmp(opcode, "halt")){
@@ -317,15 +311,16 @@ int OTypeFormat(char *opcode){
 		machine_code = (7 << 22);
 	}
 	return machine_code;
+
 }
 
-/* Getting all the labels in the code */
-void initlabel(FILE *inFilePtr, char *label, char *opcode, char *arg0, char *arg1, char *arg2){
+/* Getting all the labels inthe code */
+void initLabel(FILE *inFilePtr, char *label, char *opcode, char * arg0, char *arg1, char * arg2){
 	//memory allocation for Label array
 	for(int i = 0; i < MAXLABELNUM; i++){
 		Labels[i] = (struct Label*)malloc(sizeof(struct Label));
 	}
-	/* Initialize the Lables */
+	/* Initialize the Labels */
 	for(int i = 0; i < MAXLABELNUM; i++){
 		Labels[i]->name = NULL;
 		Labels[i]->label = NULL;
@@ -333,40 +328,41 @@ void initlabel(FILE *inFilePtr, char *label, char *opcode, char *arg0, char *arg
 	labelnum = 0;
 	/* program counter */
 	int pc = 0;
+
 	while(readAndParse(inFilePtr, label, opcode, arg0, arg1, arg2)){
-		/* if there is no label in this line continue */
+		/* it ehre is no label in current line continue */
 		if(!strcmp(label, "")){
 			pc++;
 			continue;
 		}
-		/* check whether it is valid or not */
+		/* check wheter it is valid or not */
 		if(!isValidLabel(label)){
-			printf("Invalid Label");
+			printf("Invalid Label\n");
 			exit(1);
 		}
-		/* Check whether the label already exist*/
+		/* Check wheter the label already exist */
 		for(int i = 0; i < labelnum; i++){
 			if(!strcmp(label, Labels[i]->name)){
-				printf("Label already exist");
+				printf("Label already exist\n");
 				exit(1);
 			}
 		}
-		/* After passing all the validation test insert it */
+		
 		Labels[labelnum]->name = (char*)malloc(MAXLINELENGTH*sizeof(char));
 		strcpy(Labels[labelnum]->name, label);
 		Labels[labelnum]->address = pc++;
 
-		/* if it is fill value then we have to store value in label */
+		/* if .fill */
 		if(!strcmp(opcode, ".fill")){
+			/* if the argument is a number then the value of that label will be number */
 			if(isNumber(arg0)){
-				long long check = atoi(arg0);
-				if(isValidNumber(check)){
-					Labels[labelnum]->value = atoi(arg0);
-				}
-				else{
-					printf("Value out of range");
+				//To check overflow use long long as this is 64 bits
+				long long check = atoll(arg0);
+				if(check > 2147483647 || check < -2147483648){
+					printf(".fill out of range\n");
 					exit(1);
 				}
+				Labels[labelnum]->value = atoi(arg0);
 			}
 			else{
 				Labels[labelnum]->label = (char*)malloc(MAXLINELENGTH*sizeof(char));
@@ -376,71 +372,52 @@ void initlabel(FILE *inFilePtr, char *label, char *opcode, char *arg0, char *arg
 		labelnum++;
 	}
 	/* Rewind after initializing labels */
+	for(int i = 0; i < labelnum; i++){
+		if(Labels[i]->label != NULL){
+			Labels[i]->value = getLabelAddress(Labels[i]->label);
+			free(Labels[i]->label);
+		}
+	}
 	rewind(inFilePtr);
 }
-
 
 int isValidLabel(char *name){
 	/* Maximum length of label is 6 */
 	if(strlen(name) > 6){
 		return 0;
 	}
-	/* Starts with letter*/
-	if(!isLetter(&name[0])){
+	/* Starts with number */
+	if(isNumber(&name[0])){
 		return 0;
 	}
-	/* Consist of not only letter and number */
+	/* Consist of only letters and numbers */
 	for(int i = 0; i < strlen(name); i++){
-		if(!(isNumber(&name[i])) && !(isLetter(&name[i]))){
-			return 0;
+		/* both not number and letter */
+		if(!(isNumber(&name[i]))){
+			if((name[i] < 'a' || name[i] > 'z') && (name[i] < 'A' || name[i] > 'Z')){
+				return 0;
+			}
 		}
 	}
 	return 1;
 }
 
-int isLetter(char *alphabet){
-	if(*alphabet >= 'a' && *alphabet <= 'z'){
-		return 1;
-	}
-	if(*alphabet >= 'A' && *alphabet <= 'Z'){
-		return 1;
-	}
-	return 0;
-}
-
-/* for the arguments in .fill instruction */
-/* To check that value is in the range, we need more bits than 32 */
-int isValidNumber(long long num){
-	if(num > 2147483647 || num < -2147483648){
-		return 0;
-	}
-	return 1;
-}
-
-int getLabelValue(char *label){
+int getLabelAddress(char *name){
 	for(int i = 0; i < labelnum; i++){
-		if(!strcmp(Labels[i]->name, label)){
-			if(Labels[i]->label == NULL){
-				return Labels[i]->value;
-			}
-			else{
-				return getLabelAddress(Labels[i]->label);
-			}
-		}
-	}
-	printf("label not foudn");
-	exit(1);
-}
-
-int getLabelAddress(char *label){
-	for(int i = 0; i < labelnum; i++){
-		if(!strcmp(Labels[i]->name, label)){
+		if(!strcmp(Labels[i]->name, name)){
 			return Labels[i]->address;
 		}
 	}
-	printf("label not found");
+	printf("Label not found\n");
 	exit(1);
 }
 
-
-
+int getLabelValue(char *name){
+	for(int i = 0; i < labelnum; i++){
+		if(!strcmp(Labels[i]->name, name)){
+			return Labels[i]->value;
+		}
+	}
+	printf("Label not found\n");
+	exit(1);
+}
--- project1/simulator/simulator.c
+++ project1/simulator/simulator.c
@@ -46,7 +46,93 @@ int main(int argc, char *argv[])
         printf("memory[%d]=%d\n", state.numMemory, state.mem[state.numMemory]);
     }
 
-		/* TODO: */
+    /* initialize state */
+    state.pc = 0;
+    for(int i = 0; i < NUMREGS; i++){
+        state.reg[i] = 0;
+    }
+    int instructions = 0;
+
+    printState(&state);
+
+    while(1){
+        /* Get opcode from memory */
+        int opcode = (state.mem[state.pc] >> 22) & 7;
+        /* R type format add | nor (0 or 1)*/
+        if(opcode == 0 || opcode == 1){
+            int reg1 = (state.mem[state.pc] >> 19) & 7;
+            int reg2 = (state.mem[state.pc] >> 16) & 7;
+            int destReg = (state.mem[state.pc]) & 7;
+            /* if instruction is add */
+            if(opcode == 0){
+                state.reg[destReg] = state.reg[reg1] + state.reg[reg2];
+            }
+            /* if instruction is nor */
+            else if(opcode == 1){
+                state.reg[destReg] = ~(state.reg[reg1] | state.reg[reg2]);
+            }
+        }
+        /* I type format lw | sw | beq (2, 3, 4) */
+        else if(opcode == 2 || opcode == 3 || opcode == 4){
+            int reg1 = (state.mem[state.pc] >> 19) & 7;
+            int reg2 = (state.mem[state.pc] >> 16) & 7;
+            int offset = (state.mem[state.pc] & 0xFFFF);
+            offset = convertNum(offset);
+            /* if instruction is lw */
+            if(opcode == 2){
+                state.reg[reg2] = state.mem[state.reg[reg1] + offset];
+            }
+            /* if instruction is sw */
+            else if(opcode == 3){
+                state.mem[state.reg[reg1] + offset] = state.reg[reg2];
+            }
+            /* if instruction is beq */
+            else if(opcode == 4){
+                if(state.reg[reg1] == state.reg[reg2]){
+                    instructions++;
+                    state.pc = state.pc + 1 + offset;
+                    printState(&state);
+                    continue;
+                }
+                else{
+                    instructions++;
+                    state.pc++;
+                    printState(&state);
+                    continue;
+                }
+            }
+        }
+        /* J type format jalr (5) */
+        else if(opcode == 5){
+            int reg1 = (state.mem[state.pc] >> 19) & 7;
+            int reg2 = (state.mem[state.pc] >> 16) & 7;
+            state.reg[reg2] = state.pc + 1;
+            state.pc = state.reg[reg1];
+            instructions++;
+            printState(&state);
+            continue;
+        }
+        /* O type format halt | noop */
+        else if(opcode == 6){
+            state.pc++;
+            instructions++;
+            break;
+        }
+        else if(opcode == 7){
+            state.pc++;
+            instructions++;
+            printState(&state);
+            continue;
+        }
+        instructions++;
+        state.pc++;
+        printState(&state);
+    }
+    printf("machine halted\n");
+    printf("total of %d instructions executed\n", instructions);
+    printf("final state of machine:\n");
+    printState(&state);
+
     return(0);
 }
 
